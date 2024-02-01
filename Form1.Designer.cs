1a12dc065b85aa020a02ccb99fe68ad82e88867d
megator <megator@DESKTOP-3OICMOH>
2024-01-31 21:03:30 -0400

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,298 @@
+﻿namespace InterfazGrafica_Ejercicio3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtP_H = new System.Windows.Forms.TextBox();
+            this.txtSueldoNeto = new System.Windows.Forms.TextBox();
+            this.txtTotalDescuentos = new System.Windows.Forms.TextBox();
+            this.txtSFS = new System.Windows.Forms.TextBox();
+            this.txtAFP = new System.Windows.Forms.TextBox();
+            this.txtH_T = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.btnCerrar = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.txtSueldoBruto = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(332, 23);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(309, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "SUELDO NETO DE EMPLEADO";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(187, 104);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Pago por hora";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(478, 104);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(121, 18);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Horas trabajadas";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(254, 185);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(36, 18);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "AFP";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(562, 186);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(37, 18);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "SFS";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(432, 316);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(122, 18);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Total descuentos";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(445, 384);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(87, 18);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Sueldo neto";
+            // 
+            // txtP_H
+            // 
+            this.txtP_H.Location = new System.Drawing.Point(304, 103);
+            this.txtP_H.Margin = new System.Windows.Forms.Padding(4);
+            this.txtP_H.Name = "txtP_H";
+            this.txtP_H.Size = new System.Drawing.Size(132, 22);
+            this.txtP_H.TabIndex = 7;
+            // 
+            // txtSueldoNeto
+            // 
+            this.txtSueldoNeto.Enabled = false;
+            this.txtSueldoNeto.Location = new System.Drawing.Point(422, 407);
+            this.txtSueldoNeto.Margin = new System.Windows.Forms.Padding(4);
+            this.txtSueldoNeto.Name = "txtSueldoNeto";
+            this.txtSueldoNeto.ReadOnly = true;
+            this.txtSueldoNeto.Size = new System.Drawing.Size(132, 22);
+            this.txtSueldoNeto.TabIndex = 8;
+            // 
+            // txtTotalDescuentos
+            // 
+            this.txtTotalDescuentos.Enabled = false;
+            this.txtTotalDescuentos.Location = new System.Drawing.Point(422, 340);
+            this.txtTotalDescuentos.Margin = new System.Windows.Forms.Padding(4);
+            this.txtTotalDescuentos.Name = "txtTotalDescuentos";
+            this.txtTotalDescuentos.ReadOnly = true;
+            this.txtTotalDescuentos.Size = new System.Drawing.Size(132, 22);
+            this.txtTotalDescuentos.TabIndex = 9;
+            // 
+            // txtSFS
+            // 
+            this.txtSFS.Enabled = false;
+            this.txtSFS.Location = new System.Drawing.Point(607, 185);
+            this.txtSFS.Margin = new System.Windows.Forms.Padding(4);
+            this.txtSFS.Name = "txtSFS";
+            this.txtSFS.ReadOnly = true;
+            this.txtSFS.Size = new System.Drawing.Size(132, 22);
+            this.txtSFS.TabIndex = 10;
+            // 
+            // txtAFP
+            // 
+            this.txtAFP.Enabled = false;
+            this.txtAFP.Location = new System.Drawing.Point(304, 185);
+            this.txtAFP.Margin = new System.Windows.Forms.Padding(4);
+            this.txtAFP.Name = "txtAFP";
+            this.txtAFP.ReadOnly = true;
+            this.txtAFP.Size = new System.Drawing.Size(132, 22);
+            this.txtAFP.TabIndex = 11;
+            // 
+            // txtH_T
+            // 
+            this.txtH_T.Location = new System.Drawing.Point(607, 104);
+            this.txtH_T.Margin = new System.Windows.Forms.Padding(4);
+            this.txtH_T.Name = "txtH_T";
+            this.txtH_T.Size = new System.Drawing.Size(132, 22);
+            this.txtH_T.TabIndex = 12;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.BackColor = System.Drawing.Color.ForestGreen;
+            this.btnCalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcular.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnCalcular.Location = new System.Drawing.Point(286, 456);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(121, 45);
+            this.btnCalcular.TabIndex = 13;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = false;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // btnCerrar
+            // 
+            this.btnCerrar.BackColor = System.Drawing.Color.ForestGreen;
+            this.btnCerrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCerrar.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnCerrar.Location = new System.Drawing.Point(584, 456);
+            this.btnCerrar.Name = "btnCerrar";
+            this.btnCerrar.Size = new System.Drawing.Size(112, 45);
+            this.btnCerrar.TabIndex = 14;
+            this.btnCerrar.Text = "Cerrar";
+            this.btnCerrar.UseVisualStyleBackColor = false;
+            this.btnCerrar.Click += new System.EventHandler(this.btnCerrar_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.BackColor = System.Drawing.Color.ForestGreen;
+            this.btnLimpiar.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpiar.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnLimpiar.Location = new System.Drawing.Point(435, 456);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(119, 45);
+            this.btnLimpiar.TabIndex = 15;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = false;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // txtSueldoBruto
+            // 
+            this.txtSueldoBruto.Enabled = false;
+            this.txtSueldoBruto.Location = new System.Drawing.Point(422, 270);
+            this.txtSueldoBruto.Margin = new System.Windows.Forms.Padding(4);
+            this.txtSueldoBruto.Name = "txtSueldoBruto";
+            this.txtSueldoBruto.ReadOnly = true;
+            this.txtSueldoBruto.Size = new System.Drawing.Size(132, 22);
+            this.txtSueldoBruto.TabIndex = 17;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(445, 248);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(92, 18);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "Sueldo bruto";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(993, 572);
+            this.Controls.Add(this.txtSueldoBruto);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnCerrar);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtH_T);
+            this.Controls.Add(this.txtAFP);
+            this.Controls.Add(this.txtSFS);
+            this.Controls.Add(this.txtTotalDescuentos);
+            this.Controls.Add(this.txtSueldoNeto);
+            this.Controls.Add(this.txtP_H);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Calculo de sueldo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtP_H;
+        private System.Windows.Forms.TextBox txtSueldoNeto;
+        private System.Windows.Forms.TextBox txtTotalDescuentos;
+        private System.Windows.Forms.TextBox txtSFS;
+        private System.Windows.Forms.TextBox txtAFP;
+        private System.Windows.Forms.TextBox txtH_T;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Button btnCerrar;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.TextBox txtSueldoBruto;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
